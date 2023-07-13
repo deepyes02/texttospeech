ec7e4e2fd37dad95a3f4be978b473153012c691a
deepyes02 <deepesh.dhakal@digitalwallet.co.jp>
2023-07-13 11:24:02 +0900

    script is able to execute python via php

--- invoker.php
+++ invoker.php
@@ -1,5 +1,5 @@
 <?php
 
-$command = escapeshellcmd('/Users/deepeshdhakal/Desktop/text-to-speech/text-to-speech.py');
+$command = escapeshellcmd("/Users/deepeshdhakal/Desktop/text-to-speech/text-to-speech.py 'जिन्दगी छोटो छ' 'ne'");
 $output = shell_exec($command);
 echo $output;
--- text-to-speech.py
+++ text-to-speech.py
@@ -1,9 +1,10 @@
 #!/opt/homebrew/bin/python3
-from gtts import gTTS;
+from gtts import gTTS
+import sys
 
 def textToSpeech():
-	myText = "जिन्दगी छोटो छ, तर ब्रम्हान्डको आयु लामो । यो पृथिविमा मानव जातिको उत्पत्ती भएको कती नै भयो र ? भनिन्छ, यदी पृथिवी मा जिवन सुरु वएदेखी अहिले सम्म जम्मा १२ घण्टा भैदिएको भए हामी मानव जाती उत्पन्न भएको १ मिनट अगाडी । घमन्ड नगरौ ।";
-	language = "ne"
+	myText = sys.argv[1]
+	language = sys.argv[2];
 	myObject = gTTS(text=myText, lang=language, slow=False)
 	myObject.save('hello-world.mp3')
 	print('done');
